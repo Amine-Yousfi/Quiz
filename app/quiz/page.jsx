a8a396211fa181d9ffca01917fc510e9438e0a19
ayoub yousfi <ayoub-yousfi@live.com>
2024-01-28 13:36:47 +0100

    second commit

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -1,40 +1,40 @@
-'use client';
-import React, { useState } from 'react';
-import { quiz } from '../data.js';
+"use client";
+import React, { useState } from "react";
+import { quiz } from "../data.js";
 
-const page = () => {
+const Page = () => {
   const [activeQuestion, setActiveQuestion] = useState(0);
-  const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [selectedAnswer, setSelectedAnswer] = useState("");
   const [checked, setChecked] = useState(false);
   const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
   const [showResult, setShowResult] = useState(false);
+  const [showAnswers, setShowAnswers] = useState(false);
   const [result, setResult] = useState({
     score: 0,
     correctAnswers: 0,
     wrongAnswers: 0,
   });
 
-  const { questions } = quiz;
+  const [questions, setQuestions] = useState(
+    quiz.questions.map((q) => ({ ...q, userAnswer: null }))
+  );
+
   const { question, answers, correctAnswer } = questions[activeQuestion];
 
-  //   Select and check answer
+  const handleShowAnswers = () => {
+    setShowAnswers(true);
+  };
+
+  const handleHideAnswers = () => {
+    setShowAnswers(false);
+  };
+
   const onAnswerSelected = (answer, idx) => {
     setChecked(true);
     setSelectedAnswerIndex(idx);
-    if (answer === correctAnswer) {
-      setSelectedAnswer(true);
-      console.log('true');
-    } else {
-      setSelectedAnswer(false);
-      console.log('false');
-    }
-  };
 
-  // Calculate score and increment to next question
-  const nextQuestion = () => {
-    setSelectedAnswerIndex(null);
     setResult((prev) =>
-      selectedAnswer
+      answer === correctAnswer
         ? {
             ...prev,
             score: prev.score + 5,
@@ -45,17 +45,30 @@ const page = () => {
             wrongAnswers: prev.wrongAnswers + 1,
           }
     );
-    if (activeQuestion !== questions.length - 1) {
-      setActiveQuestion((prev) => prev + 1);
-    } else {
-      setActiveQuestion(0);
-      setShowResult(true);
+
+    // Mettez à jour la réponse de l'utilisateur dans le tableau 'questions'
+    setQuestions((prevQuestions) => {
+      const updatedQuestions = [...prevQuestions];
+      updatedQuestions[activeQuestion].userAnswer = answer;
+      return updatedQuestions;
+    });
+  };
+
+  const nextQuestion = () => {
+    setSelectedAnswerIndex(null);
+    if (checked) {
+      if (activeQuestion !== questions.length - 1) {
+        setActiveQuestion((prev) => prev + 1);
+      } else {
+        setActiveQuestion(0);
+        setShowResult(true);
+      }
     }
     setChecked(false);
   };
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Quiz Page</h1>
       <div>
         <h2>
@@ -65,34 +78,32 @@ const page = () => {
       </div>
       <div>
         {!showResult ? (
-          <div className='quiz-container'>
+          <div className="quiz-container">
             <h3>{questions[activeQuestion].question}</h3>
             {answers.map((answer, idx) => (
               <li
                 key={idx}
                 onClick={() => onAnswerSelected(answer, idx)}
                 className={
-                  selectedAnswerIndex === idx ? 'li-selected' : 'li-hover'
+                  selectedAnswerIndex === idx ? "li-selected" : "li-hover"
                 }
               >
                 <span>{answer}</span>
               </li>
             ))}
             {checked ? (
-              <button onClick={nextQuestion} className='btn'>
-                {activeQuestion === question.length - 1 ? 'Finish' : 'Next'}
+              <button onClick={nextQuestion} className="btn">
+                {activeQuestion === questions.length - 1 ? "Finish" : "Next"}
               </button>
             ) : (
-              <button onClick={nextQuestion} disabled className='btn-disabled'>
-                {' '}
-                {activeQuestion === question.length - 1 ? 'Finish' : 'Next'}
+              <button onClick={nextQuestion} disabled className="btn-disabled">
+                {activeQuestion === questions.length - 1 ? "Finish" : "Next"}
               </button>
             )}
           </div>
         ) : (
-          <div className='quiz-container'>
+          <div className="quiz-container">
             <h3>Results</h3>
-            
             <p>
               Total Questions: <span>{questions.length}</span>
             </p>
@@ -105,7 +116,40 @@ const page = () => {
             <p>
               Wrong Answers: <span>{result.wrongAnswers}</span>
             </p>
+            {showAnswers && (
+              <div>
+                <ul>
+                  {questions.map((q, index) => (
+                    <li key={index}>
+                      <strong>{q.question}</strong>
+                      <br />
+                      <br />
+                      <strong>Correct Answer:</strong> {q.correctAnswer}
+                      <br />
+                      <br />
+                      <span
+                        style={{
+                          color:
+                            q.userAnswer === q.correctAnswer ? "green" : "red",
+                        }}
+                      >
+                        <strong>Your Answer:</strong> {q.userAnswer}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        )}
+        {showResult && (
+          <div>
             <button onClick={() => window.location.reload()}>Restart</button>
+            <button
+              onClick={showAnswers ? handleHideAnswers : handleShowAnswers}
+            >
+              {showAnswers ? "Hide Answers" : "Show Answers"}
+            </button>
           </div>
         )}
       </div>
@@ -113,4 +157,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
